2b7a908cf8b13bc7b236f0afa2a1ba5928e64f3d
Zhen Liu <liuzhen1994@hotmail.com>
2023-04-20 11:07:22 +0200

    Update save_meta.py

--- metadata/save_meta.py
+++ metadata/save_meta.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     parser.add_argument('--json_path', type=str)
     args = parser.parse_args()
 
-    fpath_list = sorted([os.path.join(args.data_path, fname) for fname in os.listdir(root) if fname.endswith('.pt')])
+    fpath_list = sorted([os.path.join(args.data_path, fname) for fname in os.listdir(args.data_path) if fname.endswith('.pt')])
     os.makedirs(args.json_path, exist_ok=True)
     json.dump(fpath_list, open(args.json_path, 'w'))
 
